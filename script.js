45724645ba3e87f148a5681f0f18da9aa26a37b8
FES001 <firdavs7794948@gmail.com>
2024-08-30 22:24:15 +0500

    Almost

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const toggleBtn = document.getElementById('toggleBtn');
+const monthlyYearlyTexts = document.querySelector('.plan-yearly-monthly-price');
 
 toggleBtn.addEventListener('click', () => {
     toggleBtn.classList.toggle('toggle-btn-js');
+    monthlyYearlyTexts.classList.toggle('price-container-js');
 });
